84ece238ccba71a75575dab97529b7b54174ee24
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using DevExpress.Data;
+using System.Collections.Generic;
+using DevExpress.Xpf.Grid;
+
+namespace DragAndDrop_ReorderAndExternal {
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            grid.ItemsSource = TestData.GetData();
+            draggedRowsList.ItemsSource = new BindingList<TestData>();
+        }
+    }
+
+    #region TestData class
+    public class TestData {
+        public static BindingList<TestData> GetData(int count = 100) {
+            BindingList<TestData> list = new BindingList<TestData>();
+            for(int i = 0; i < count; i++)
+                list.Add(new TestData(i, "row " + i));
+            return list;
+        }
+        public string Text { get; set; }
+        public int Number { get; set; }
+        public TestData() { }
+        public TestData(int number, string text) {
+            Number = number;
+            Text = text;
+        }
+        public override string ToString() {
+            return Text;
+        }
+    }
+    #endregion
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,58 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.ComponentModel
+Imports System.Windows
+Imports System.Windows.Input
+Imports DevExpress.Data
+Imports System.Collections.Generic
+Imports DevExpress.Xpf.Grid
+
+Namespace DragAndDrop_ReorderAndExternal
+
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = TestData.GetData()
+			draggedRowsList.ItemsSource = New BindingList(Of TestData)()
+		End Sub
+	End Class
+
+	#Region "TestData class"
+	Public Class TestData
+		Public Shared Function GetData(Optional ByVal count As Integer = 100) As BindingList(Of TestData)
+			Dim list As New BindingList(Of TestData)()
+			For i As Integer = 0 To count - 1
+				list.Add(New TestData(i, "row " & i))
+			Next i
+			Return list
+		End Function
+		Private privateText As String
+		Public Property Text() As String
+			Get
+				Return privateText
+			End Get
+			Set(ByVal value As String)
+				privateText = value
+			End Set
+		End Property
+		Private privateNumber As Integer
+		Public Property Number() As Integer
+			Get
+				Return privateNumber
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber = value
+			End Set
+		End Property
+		Public Sub New()
+		End Sub
+		Public Sub New(ByVal number As Integer, ByVal text As String)
+			Number = number
+			Text = text
+		End Sub
+		Public Overrides Function ToString() As String
+			Return Text
+		End Function
+	End Class
+	#End Region
+End Namespace
