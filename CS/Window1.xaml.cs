6e161eba9a8677e837412eac8511aea9804d1c41
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using DevExpress.Data;
+
+namespace DragAndDrop_ReorderAndExternal {
+
+    public partial class Window1 : Window {
+        BindingList<TestData> list;
+        BindingList<TestData> draggedRows = new BindingList<TestData>();
+        bool dragStarted;
+
+        public Window1() {
+            InitializeComponent();
+
+            #region Data binding
+            list = new BindingList<TestData>();
+            for (int i = 0; i < 100; i++) {
+                list.Add(new TestData() {
+                    Text = "row " + i,
+                    Number = i,
+                });
+            }
+            grid.ItemsSource = list;
+            #endregion
+
+            view.AllowDrop = true;
+            draggedRowsList.AllowDrop = true;
+
+            view.PreviewMouseDown += new MouseButtonEventHandler(View_PreviewMouseDown);
+            view.DragOver += new DragEventHandler(View_DragOver);
+            view.Drop += new DragEventHandler(View_Drop);
+
+            draggedRowsList.DragOver += new DragEventHandler(draggedRowsList_DragOver);
+            draggedRowsList.Drop += new DragEventHandler(draggedRowsList_Drop);
+            draggedRowsList.ItemsSource = draggedRows;
+        }
+
+        void draggedRowsList_Drop(object sender, DragEventArgs e) {
+            int rowHandle = (int)e.Data.GetData(typeof(int));
+            TestData obj = (TestData)grid.GetRow(rowHandle);
+            if (!draggedRows.Contains(obj))
+                draggedRows.Add(obj);
+        }
+
+        void draggedRowsList_DragOver(object sender, DragEventArgs e) {
+            int rowHandle = (int)e.Data.GetData(typeof(int));
+            TestData obj = (TestData)grid.GetRow(rowHandle);
+            e.Effects = draggedRows.Contains(obj) ? DragDropEffects.None : DragDropEffects.Move;
+            e.Handled = true;
+        }
+
+        void View_Drop(object sender, DragEventArgs e) {
+            int rowHandle = (int)e.Data.GetData(typeof(int));
+            TestData obj = (TestData)grid.GetRow(rowHandle);
+            int insertRowHandle = view.GetRowHandleByTreeElement(e.OriginalSource as DependencyObject);
+            int insertListIndex = grid.GetRowListIndex(insertRowHandle);
+            if (IsCopyEffect(e)) {
+                list.Insert(insertListIndex, new TestData() { Text = obj.Text + " (Copy)", Number = obj.Number });
+            }
+            else {
+                list.Remove(obj);
+                list.Insert(insertListIndex, obj);
+            }
+        }
+
+        void View_DragOver(object sender, DragEventArgs e) {
+            if (view.GetRowElementByTreeElement(e.OriginalSource as DependencyObject) != null)
+                e.Effects = IsCopyEffect(e) ? DragDropEffects.Copy : DragDropEffects.Move;
+            else
+                e.Effects = DragDropEffects.None;
+            e.Handled = true;
+        }
+
+        bool IsCopyEffect(DragEventArgs e) {
+            return (e.KeyStates & DragDropKeyStates.ControlKey) == DragDropKeyStates.ControlKey;
+        }
+
+        private void view_MouseMove(object sender, MouseEventArgs e) {
+            int rowHandle = view.GetRowHandleByMouseEventArgs(e);
+            if (dragStarted) {
+                DataObject data = CreateDataObject(rowHandle);
+                DragDrop.DoDragDrop(view.GetRowElementByMouseEventArgs(e), data,
+                    DragDropEffects.Move | DragDropEffects.Copy);
+                dragStarted = false;
+            }
+        }
+
+        void View_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            int rowHandle = view.GetRowHandleByMouseEventArgs(e);
+            if (rowHandle != GridDataController.InvalidRow)
+                dragStarted = true;
+        }
+
+        private DataObject CreateDataObject(int rowHandle) {
+            DataObject data = new DataObject();
+            data.SetData(typeof(int), rowHandle);
+            return data;
+        }
+    }
+
+    #region TestData class
+    public class TestData {
+        public string Text { get; set; }
+        public int Number { get; set; }
+    }
+    #endregion
+}
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,125 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.ComponentModel
+Imports System.Windows
+Imports System.Windows.Input
+Imports DevExpress.Data
+
+Namespace DragAndDrop_ReorderAndExternal
+
+	Partial Public Class Window1
+		Inherits Window
+		Private list As BindingList(Of TestData)
+		Private draggedRows As New BindingList(Of TestData)()
+		Private dragStarted As Boolean
+
+		Public Sub New()
+			InitializeComponent()
+
+'			#Region "Data binding"
+			list = New BindingList(Of TestData)()
+			For i As Integer = 0 To 99
+				list.Add(New TestData() With {.Text = "row " & i, .Number = i})
+			Next i
+			grid.ItemsSource = list
+'			#End Region
+
+			view.AllowDrop = True
+			draggedRowsList.AllowDrop = True
+
+			AddHandler view.PreviewMouseDown, AddressOf View_PreviewMouseDown
+			AddHandler view.PreviewMouseMove, AddressOf View_PreviewMouseMove
+			AddHandler view.DragOver, AddressOf View_DragOver
+			AddHandler view.Drop, AddressOf View_Drop
+
+			AddHandler draggedRowsList.DragOver, AddressOf draggedRowsList_DragOver
+			AddHandler draggedRowsList.Drop, AddressOf draggedRowsList_Drop
+			draggedRowsList.ItemsSource = draggedRows
+		End Sub
+
+		Private Sub draggedRowsList_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
+			Dim rowHandle As Integer = CInt(Fix(e.Data.GetData(GetType(Integer))))
+			Dim obj As TestData = CType(grid.GetRow(rowHandle), TestData)
+			If (Not draggedRows.Contains(obj)) Then
+				draggedRows.Add(obj)
+			End If
+		End Sub
+
+		Private Sub draggedRowsList_DragOver(ByVal sender As Object, ByVal e As DragEventArgs)
+			Dim rowHandle As Integer = CInt(Fix(e.Data.GetData(GetType(Integer))))
+			Dim obj As TestData = CType(grid.GetRow(rowHandle), TestData)
+			e.Effects = If(draggedRows.Contains(obj), DragDropEffects.None, DragDropEffects.Move)
+			e.Handled = True
+		End Sub
+
+		Private Sub View_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
+			Dim rowHandle As Integer = CInt(Fix(e.Data.GetData(GetType(Integer))))
+			Dim obj As TestData = CType(grid.GetRow(rowHandle), TestData)
+			Dim insertRowHandle As Integer = view.GetRowHandleByTreeElement(TryCast(e.OriginalSource, DependencyObject))
+			Dim insertListIndex As Integer = grid.GetRowListIndex(insertRowHandle)
+			If IsCopyEffect(e) Then
+				list.Insert(insertListIndex, New TestData() With {.Text = obj.Text & " (Copy)", .Number = obj.Number})
+			Else
+				list.Remove(obj)
+				list.Insert(insertListIndex, obj)
+			End If
+		End Sub
+
+		Private Sub View_DragOver(ByVal sender As Object, ByVal e As DragEventArgs)
+			If view.GetRowElementByTreeElement(TryCast(e.OriginalSource, DependencyObject)) IsNot Nothing Then
+				e.Effects = If(IsCopyEffect(e), DragDropEffects.Copy, DragDropEffects.Move)
+			Else
+				e.Effects = DragDropEffects.None
+			End If
+			e.Handled = True
+		End Sub
+
+		Private Function IsCopyEffect(ByVal e As DragEventArgs) As Boolean
+			Return (e.KeyStates And DragDropKeyStates.ControlKey) = DragDropKeyStates.ControlKey
+		End Function
+
+		Private Sub View_PreviewMouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+			Dim rowHandle As Integer = view.GetRowHandleByMouseEventArgs(e)
+			If dragStarted Then
+				Dim data As DataObject = CreateDataObject(rowHandle)
+				DragDrop.DoDragDrop(view.GetRowElementByMouseEventArgs(e), data, DragDropEffects.Move Or DragDropEffects.Copy)
+				dragStarted = False
+			End If
+		End Sub
+
+		Private Sub View_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+			Dim rowHandle As Integer = view.GetRowHandleByMouseEventArgs(e)
+			If rowHandle <> GridDataController.InvalidRow Then
+				dragStarted = True
+			End If
+		End Sub
+
+		Private Function CreateDataObject(ByVal rowHandle As Integer) As DataObject
+			Dim data As New DataObject()
+			data.SetData(GetType(Integer), rowHandle)
+			Return data
+		End Function
+	End Class
+
+	#Region "TestData class"
+	Public Class TestData
+		Private privateText As String
+		Public Property Text() As String
+			Get
+				Return privateText
+			End Get
+			Set(ByVal value As String)
+				privateText = value
+			End Set
+		End Property
+		Private privateNumber As Integer
+		Public Property Number() As Integer
+			Get
+				Return privateNumber
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber = value
+			End Set
+		End Property
+	End Class
+	#End Region
+End Namespace
